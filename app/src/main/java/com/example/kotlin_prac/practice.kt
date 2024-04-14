54d8d4889c6513adf576eb699aee199c8d1409e4
Abid <abidroid1@gmail.com>
2024-04-14 16:48:39 +0500

    variables and datatypes

--- app/src/main/java/com/example/kotlin_prac/practice.kt
+++ app/src/main/java/com/example/kotlin_prac/practice.kt
@@ -1,5 +1,24 @@
 package com.example.kotlin_prac
 
 fun main() {
+    // for outputs
     println("Hello World")
+
+    // val is used to declare constants
+    val pi: Float = 3.14f
+    val gpa: Double = 3.3
+
+    // var is used to declare variables
+    var age: Int = 30
+    var comments: Int = 484
+
+    // all datatypes
+    var count: Byte = 1
+    var countries : Short = 200
+    var salary: Int = 50000
+    var pakDebt: Long = 838383883838383
+
+    var name: String = "Abid"
+    var isMarried: Boolean = true
+
 }
